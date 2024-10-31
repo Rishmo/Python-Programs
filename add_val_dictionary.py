95c4e19766e004d5b37a204822f45769111bbab3
Rishmo <100657272+Rishmo@users.noreply.github.com>
2024-10-31 19:45:39 +0530

    Add files via upload

---
+++ add_val_dictionary.py
@@ -0,0 +1,8 @@
+#Adding values to dictionary
+d={}
+d['James']=78
+print(d)
+d.update({'Harry':42})#if key present updates value else add key to dictionary
+print(d)
+d.setdefault('Syam',26)
+print(d)
